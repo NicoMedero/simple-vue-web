c97e5cda217fcbf07e896fd249760eaf0c8de912
Mede <nicolasmederoc@gmail.com>
2021-04-12 20:51:01 -0300

    add a navbar from bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/navbar/Navbar.vue'
+import Header from './components/header/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./main.css" />
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="navbar">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand class="navbar-main-title" href="#">Vendemos Todo</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto navbar-items">
+                    <b-nav-item href="#">Tecno</b-nav-item>
+                    <b-nav-item href="#">Forniture</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+  </div>
+</template>
+
+<script src="./navbar.js" />
+
+<style src="./navbar.css" />
